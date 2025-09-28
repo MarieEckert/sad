13f0e632b974922667391268e9c108afcbfd5d80
Marie <marie@ha.si>
2025-09-28 22:15:17 +0200

    remove old files

--- sadhtml/inc/uSADParser.pp
+++
@@ -1,1 +0,0 @@
-../../src/uSADParser.pp
--- sadv/windows_install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-mkdir out
-fpc src\sadv.pp -O4 -XX -Xs -FE"out/" -Fu"../src/" -Fu"../inc/" -Fu"inc/"
-
-if not exist "C:\Program Files\sadv" mkdir "C:\Program Files\sadv"
-mv out\sadv.exe "C:\Program Files\sadv\sadv.exe"
-
-pause
